e0c8e35302de6de5f1255777ce408b8d63325dcb
Cristhian Carbonell <alexcarbo8809@gmail.com>
2022-09-27 19:27:55 +0000

    files

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from enum import Enum
 from pydantic import BaseModel, Field, EmailStr
 #FastAPI
 from fastapi import FastAPI, Body, Query, Path, status, Form, Header, Cookie
+from fastapi import UploadFile, File
 
 app = FastAPI()
 
@@ -217,3 +218,19 @@ def contact(
     ads: Optional[str] = Cookie(default=None)
 ):
     return user_agent
+
+# Files
+
+@app.post(
+    path="/post-image"
+)
+def post_image(
+    image: UploadFile = File(
+        ...,
+    )
+):
+    return {
+        "Filename": image.filename,
+        "Format":image.content_type,
+        "Size(kb)": round(len(image.file.read()) / 1024, ndigits=2)
+    }
